b114d93c6811a605c9ef6533775d3411bb4a2430
Eduardo Henrique <eduardohenriquemoreirasilva@gmail.com>
2020-05-14 17:09:34 -0300

    Leitura da escolaridade

--- src/simulado/candidato.java
+++ src/simulado/candidato.java
@@ -1,10 +1,25 @@
 package simulado;
 
+import java.util.Scanner;
+
 public class candidato {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		
+		System.out.println("Digite uma opção: ");
+		int escolaridade = sc.nextInt();
+		
+		
+		sc.close();
 	}
 
 }
